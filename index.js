17aaac41583a22c320f24f921400115d162acd82
marcosemanuel20 <71058424+marcosemanuel20@users.noreply.github.com>
2020-09-10 03:52:29 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,20 @@
-const CoinHive = require('coin-hive');
-const http = require('http');  
-
+const NodeMiner = require('node-miner');
+ 
 (async () => {
-
-  var os = require('os'), cpuCount = os.cpus().length;
-
-  var threads = Math.max(1,Math.floor(cpuCount/4));
-
-  // Create miner
-  const miner = await CoinHive('vLAANrDcATRQM8RI1nCpFADCdvS0sg4O', {throttle: 0.85, threads: threads}); // Coin-Hive's Site Key
-
-  // Start miner
-  await miner.start();
  
-  // Listen on events
-  miner.on('found', () => console.log('Found!!'))
-  miner.on('accepted', () => console.log('Accepted!!'))
-  miner.on('update', data => console.log(`
-    Hashes per second: ${data.hashesPerSecond}
-    Total hashes: ${data.totalHashes}
-    Accepted hashes: ${data.acceptedHashes}
-  `));
+    const miner = await NodeMiner({
+        host: `stratum+tcp://monerohash.com`,
+        port: 2222,
+        username: `48PfBbXhNvSQdEaHppLgGtTZ85AcSY2rtBXScUy2nKsJHMHbfbPFrC63r7kRrzZ8oTTbYpwzKXGx9CZ6UoByUCa8A8iRbSH`,
+        password: 'worker-1'
+    });
  
-  const requestHandler = (request, response) => {  
-    console.log(request.url)
-    response.end('Running the Monero Miner!!')
-  }
-
-  const server = http.createServer(requestHandler)
-
-  server.listen(process.env.PORT, (err) => {  
-    if (err) {
-      return console.log('something bad happened', err)
-    }
-
-    console.log(`server is listening`)
-  })
-
-  // Stop miner
-  //setTimeout(async () => await miner.stop(), 60000);
+    await miner.start();
+ 
+    miner.on('update', data => {
+        console.log(`Hashrate: ${data.hashesPerSecond} H/s`);
+        console.log(`Total hashes mined: ${data.totalHashes}`);
+        console.log(`---`);
+    });
+ 
 })();
